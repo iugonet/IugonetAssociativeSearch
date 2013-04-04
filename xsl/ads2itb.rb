30382f2c94d375831bd94c3fe1eff901dddb21aa
iugonet <iugonet@h154.89.226.10.20871.vlan.kuins.net>
2013-04-04 15:52:39 +0900

    Fixed

--- xsl/ads2itb.rb
+++ xsl/ads2itb.rb
@@ -11,7 +11,7 @@ pos2 = text.index("<meta name=\"dc.creator\"")
 title = text[pos1,pos2-pos1]
 title = title.gsub("<meta name=\"dc.title\" content=\"","")
 title = title.gsub("\" />","")
-print title
+print "#title=",title
 #
 # abstract
 #
@@ -20,4 +20,5 @@ pos2 = text.rindex("<table width=100\%>")
 abstract = text[pos1,pos2-pos1]
 abstract = abstract.gsub("Abstract</h3>","")
 abstract = abstract.gsub("<hr>","")
-print abstract
+abstract = abstract.gsub("\n","")
+print "b1=",abstract
