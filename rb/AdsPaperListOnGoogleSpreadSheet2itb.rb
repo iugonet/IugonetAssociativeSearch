238c6b8340ae80a3245d40ee39071b70e58ffda4
iugonet <iugonet@h155.89.226.10.20871.vlan.kuins.net>
2013-05-28 17:00:37 +0900

    Fixed

--- rb/AdsPaperListOnGoogleSpreadSheet2itb.rb
+++ rb/AdsPaperListOnGoogleSpreadSheet2itb.rb
@@ -7,7 +7,7 @@ doc = REXML::Document.new(open("../conf/GoogleAccount.xml"))
 
 USER = doc.elements['GoogleAccount/user'].text
 PASS = doc.elements['GoogleAccount/password'].text
-URL = "https://docs.google.com/spreadsheet/ccc?key=0AgbaOOcOsZncdDFYa2xHd0puZnVBVUVwakp0bzJuVkE#gid=0"
+URL = "https://docs.google.com/spreadsheet/ccc?key=0AgbaOOcOsZncdE5sOFJySXFOdnNRMXBoMnQ5MFJLRmc#gid=0"
 
 session = GoogleSpreadsheet.login(USER, PASS)
 ws = session.spreadsheet_by_url(URL).worksheets[0]
