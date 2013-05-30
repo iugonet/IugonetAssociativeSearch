cf064b7c3f9d84913e355052981c7cb54feba5d3
root <root@h155.89.226.10.20871.vlan.kuins.net>
2013-05-30 11:01:35 +0900

    Fixed

--- rb/getAdsPaperListOnGoogleSpreadSheet.rb
+++ rb/getAdsPaperListOnGoogleSpreadSheet.rb
@@ -8,6 +8,13 @@ doc = REXML::Document.new(open("../conf/GoogleAccount.xml"))
 
 USER = doc.elements['GoogleAccount/user'].text
 PASS = doc.elements['GoogleAccount/password'].text
+print "AAAA"
+print "BBBB"
+exit(1)
+if (USER == "" || PASS == "") then
+  print "Please speficy USER & PASS in GoogleAccount.xml"
+  exit(1)
+end
 URL = "https://docs.google.com/spreadsheet/ccc?key=0AgbaOOcOsZncdE5sOFJySXFOdnNRMXBoMnQ5MFJLRmc#gid=0"
 
 session = GoogleSpreadsheet.login(USER, PASS)
