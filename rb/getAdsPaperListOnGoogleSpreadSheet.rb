8d89b1b50ffef8399ad76c2d1712781bd444b2dc
root <root@h155.89.226.10.20871.vlan.kuins.net>
2013-06-03 16:54:36 +0900

    Fixed

--- rb/getAdsPaperListOnGoogleSpreadSheet.rb
+++ rb/getAdsPaperListOnGoogleSpreadSheet.rb
@@ -3,24 +3,39 @@ require 'rubygems'
 require "google_spreadsheet"
 require 'rexml/document'
 require 'net/http'
+require 'optparse'
 
 doc = REXML::Document.new(open("../conf/GoogleAccount.xml"))
-
 USER = doc.elements['GoogleAccount/user'].text
 PASS = doc.elements['GoogleAccount/password'].text
 
-if ( USER.nil? || PASS.nil? ) then
-  STDERR.puts "Please speficy USER & PASS in GoogleAccount.xml"
-  exit(1)
-end
-
 URL = "https://docs.google.com/spreadsheet/ccc?key=0AgbaOOcOsZncdE5sOFJySXFOdnNRMXBoMnQ5MFJLRmc#gid=0"
 
-session = GoogleSpreadsheet.login(USER, PASS)
-ws = session.spreadsheet_by_url(URL).worksheets[0]
+opt = OptionParser.new
+opt.on('-o','output-file-name') {|v| p v}
 
-f = open('list.txt', 'w')
-1.upto(ws.num_rows){|i|
-  f.write "http://adsabs.harvard.edu/abs/"+ws[i, 1]+"\n"
-}
-f.close
+opt.parse!(ARGV)
+
+def getAdsPaperListOnGoogleSpreadSheet()
+  if ( USER.nil? || PASS.nil? ) then
+    STDERR.puts "Please speficy USER & PASS in GoogleAccount.xml"
+    exit(1)
+  end
+
+  session = GoogleSpreadsheet.login(USER, PASS)
+  ws = session.spreadsheet_by_url(URL).worksheets[0]
+
+  f = open('list.txt', 'w')
+  1.upto(ws.num_rows){|i|
+    f.write "http://adsabs.harvard.edu/abs/"+ws[i, 1]+"\n"
+  }
+  f.close
+end
+
+if ARGV.length == 1 then
+  getAdsPaperListOnGoogleSpreadSheet()
+else
+  print "Usage: getAdsPaperListOnGoogleSpreadSheet [options]\n"
+  print "    -o                               output-file-n\n"
+end
+
--- sh/ads.sh
+++ sh/ads.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 if [ -a 'list.txt' ]; then
-    rm list.txt
+    rm /tmp/list.txt
 fi 
 
 if [ -a '/tmp/createItb.sh' ]; then
     rm /tmp/createItb.sh
 fi
 
-../rb/getAdsPaperListOnGoogleSpreadSheet.rb
+../rb/getAdsPaperListOnGoogleSpreadSheet.rb -o /tmp/list.txt
 echo "#!/usr/bin/env bash" > /tmp/createItb.sh
 echo "rm ../itb/ads.itb" >> /tmp/createItb.sh
 echo "echo '@title=ADS' >> ../itb/ads.itb" >> /tmp/createItb.sh
 echo "echo '@description=The dictionary generated from the metadata of SAO/NASA Astrophysics Data System' >> ../itb/ads.itb" >> /tmp/createItb.sh
 if [ -a 'list.txt' ]; then
-    cat list.txt | awk '{print "curl -o /tmp/index.html -O ",$0,"; ../rb/adsMetadata2itb.rb /tmp/index.html >> ../itb/ads.itb"}' >> /tmp/createItb.sh
+    cat /tmp/list.txt | awk '{print "curl -o /tmp/index.html -O ",$0,"; ../rb/adsMetadata2itb.rb /tmp/index.html >> ../itb/ads.itb"}' >> /tmp/createItb.sh
     chmod +x /tmp/createItb.sh
     rm list.txt
 fi
